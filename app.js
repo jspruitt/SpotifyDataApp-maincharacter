33a9139c2293dbb44b38618041846bb828881daa
jspruitt <jennyspruitt@gmail.com>
2020-05-20 00:11:20 -0500

    Part tres: i would say three times the charm, but I'm not that confident

--- app.js
+++ app.js
@@ -176,3 +176,6 @@ app.get('/refresh_token', function(req, res) {
 app.get('/', function(req, res){
     res.redirect("'/public/index.html'")
 });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
